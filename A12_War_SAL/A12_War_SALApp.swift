586278603401a539aa4ccd8e361f7d3572fc4388
Sean Logsdon <seanlogsdon14@gmail.com>
2020-11-11 21:03:25 -0600

    Initial Commit

---
+++ A12_War_SAL/A12_War_SALApp.swift
@@ -0,0 +1,17 @@
+//
+//  A12_War_SALApp.swift
+//  A12_War_SAL
+//
+//  Created by Sean Logsdon on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct A12_War_SALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A12_War_SAL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  A12_War_SAL
+//
+//  Created by Sean Logsdon on 11/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
